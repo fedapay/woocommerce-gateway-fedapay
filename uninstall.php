472db51f04346018fd47e60f109ca6bd8c77f697
Boris Koumondji <brexis2009@yahoo.fr>
2019-08-05 13:44:36 +0000

    Make sure to drop table and delete option

--- uninstall.php
+++ uninstall.php
@@ -5,10 +5,13 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
 
-$table_name = $wpdb->prefix . 'woo_fedapay_orders_transactions';
+global $wpdb;
+
+$table_name = $wpdb->prefix . 'wc_fedapay_orders_transactions';
 
 $charset_collate = $wpdb->get_charset_collate();
 
-$sql = "DROP TABLE IF EXISTS `$table_name` $charset_collate;";
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-dbDelta( $sql );
+$sql = "DROP TABLE IF EXISTS `$table_name`;";
+$wpdb->query( $sql );
+
+delete_option( 'wc_fedapay_db_version' );
