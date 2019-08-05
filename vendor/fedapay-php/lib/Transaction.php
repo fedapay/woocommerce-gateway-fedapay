cbc2a1bb4a41864cd9e5504b4de106e79489633f
Boris Koumondji <brexis2009@yahoo.fr>
2019-08-05 18:04:30 +0000

    Update fedapay library

--- vendor/fedapay-php/lib/Transaction.php
+++ vendor/fedapay-php/lib/Transaction.php
@@ -21,69 +21,18 @@ use FedaPay\Util\Util;
  */
 class Transaction extends Resource
 {
-    /**
-     * @param array|string $id The ID of the transaction to retrieve
-     * @param array|null $headers
-     *
-     * @return Transaction
-     */
-    public static function retrieve($id, $headers = [])
-    {
-        return self::_retrieve($id, $headers);
-    }
+    use ApiOperations\All;
+    use ApiOperations\Retrieve;
+    use ApiOperations\Create;
+    use ApiOperations\Update;
+    use ApiOperations\Save;
+    use ApiOperations\Delete;
 
-    /**
-     * @param array|null $params
-     * @param array|string|null $headers
-     *
-     * @return Collection of Transactions
-     */
-    public static function all($params = [], $headers = [])
-    {
-        return self::_all($params, $headers);
-    }
+    private static $availableMobileMoney = ['mtn', 'moov', 'mtn_ci'];
 
-    /**
-     * @param array|null $params
-     * @param array|string|null $headers
-     *
-     * @return Transaction The created transaction.
-     */
-    public static function create($params = [], $headers = [])
+    protected function modeAvailable($mode)
     {
-        return self::_create($params, $headers);
-    }
-
-    /**
-     * @param string $id The ID of the customer to update.
-     * @param array|null $params
-     * @param array|string|null $options
-     *
-     * @return Transaction The updated transaction.
-     */
-    public static function update($id, $params = [], $headers = [])
-    {
-        return self::_update($id, $params, $headers);
-    }
-
-    /**
-     * @param array|string|null $headers
-     *
-     * @return Transaction The saved transaction.
-     */
-    public function save($headers = [])
-    {
-        return $this->_save($headers);
-    }
-
-    /**
-     * @param array $headers
-     *
-     * @return void
-     */
-    public function delete($headers = [])
-    {
-        return $this->_delete($headers);
+        return in_array($mode, self::$availableMobileMoney);
     }
 
     /**
@@ -96,5 +45,37 @@ class Transaction extends Resource
 
         list($response, $opts) = static::_staticRequest('post', $url, $params, $headers);
         return Util::arrayToFedaPayObject($response, $opts);
+    }
+
+    /**
+     * Send Mobile Money request with token
+     * @return FedaPay\FedaPayObject
+     */
+    public function sendNowWithToken($mode, $token, $params = [], $headers = [])
+    {
+        if (!$this->modeAvailable($mode)) {
+            throw new \InvalidArgumentException(
+                'Invalid payment method \''.$mode.'\' supplied. '
+                .'You have to use one of the following payment methods '.
+                '['. implode(self::$availableMobileMoney, ',') .']'
+            );
+        }
+
+        $url = '/' . $mode;
+        $params = array_merge(['token' => $token], $params);
+
+        list($response, $opts) = static::_staticRequest('post', $url, $params, $headers);
+        return Util::arrayToFedaPayObject($response, $opts);
+    }
+
+    /**
+     * Send Mobile Money request
+     * @return FedaPay\FedaPayObject
+     */
+    public function sendNow($mode, $params = [], $headers = [])
+    {
+        $tokenObject = $this->generateToken([], $headers);
+
+        return $this->sendNowWithToken($mode, $tokenObject->token, $params, $headers);
     }
 }
