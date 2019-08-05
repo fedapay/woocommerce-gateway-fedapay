876dbd21bde64bb9545db4e6c19a0419958a7ce1
Boris Koumondji <brexis2009@yahoo.fr>
2019-08-05 21:06:04 +0000

    Fix deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ svn co $SVN_REPOSITORY $SVN_DIR
 rsync -r -p ./wordpress_org_assets/* $SVN_DIR/assets
 
 # 3. Clean up unnecessary files
-rm -rf wordpress_org_assets/ deploy/ .git/
+rm -rf wordpress_org_assets/ deploy/
 
 # 4. Copy git repository contents to SNV trunk/ directory
 rsync -r -p ./* $SVN_DIR/trunk/
@@ -24,7 +24,8 @@ svn stat $SVN_DIR | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
 # 7. Remove deleted files from SVN
 svn stat $SVN_DIR | grep '^!' | awk '{print $2}' | xargs -I x svn rm --force x@
 
-svn stat svn $SVN_DIR
+ls -l $SVN_DIR
+svn stat $SVN_DIR
 
 # 8. Push SVN tag
 #svn ci  --message "Release $TRAVIS_TAG" \
