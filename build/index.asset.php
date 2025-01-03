812c42b037e2994fd739e0ef20462b514168bd71
Boris Koumondji <brexis2009@yahoo.fr>
2025-01-03 17:57:15 +0000

    Run build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => '723ebf340e7a075c6a08');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wc-settings', 'wp-html-entities', 'wp-i18n'), 'version' => 'dc6cb8ac3e31b727f519');
